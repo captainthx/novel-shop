7dad537cccad9529cb3d8ee36c52d3e8ee960ce7
capptt <thanyatab.chumponrob.work@gmail.com>
2023-07-22 10:39:44 +0700

    update newApiService

--- src/services/globalService.ts
+++ src/services/globalService.ts
@@ -4,6 +4,12 @@ import type { pagination, ServerResponse, book } from '@/services/index';
 const fetchAll = (params: pagination): ServerResponse<book[]> =>
     api.get('/v1/global/books', { params });
 
+const fetchByType = (type: string, params: pagination): ServerResponse<book[]> =>
+    api.get('/v1/global/books/type', { params: { type, params } });
+
+const fetchNewBooks = (params: pagination): ServerResponse<book[]> =>
+    api.get('/v1/global/books/new', { params });
+
 const fetchById = (bookId: number): ServerResponse<book> => api.get(`/v1/global/books/` + bookId);
 
-export { fetchAll, fetchById };
+export { fetchAll, fetchByType, fetchNewBooks, fetchById };
